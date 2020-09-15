b1fb26acfeefa66c939f8be78b03452540b7de9d
hsgarzon2020 <hsgarzon2020@gmail.com>
2020-09-15 00:43:59 -0500

    Update PaymentConfirmationController.php

--- app/Http/Controllers/Payment/PaymentConfirmationController.php
+++ app/Http/Controllers/Payment/PaymentConfirmationController.php
@@ -33,7 +33,7 @@ class PaymentConfirmationController extends Controller
 
             if ($payment->status == "approved") {
 
-                //dd($shopping_cart, $payment->payer->email);
+                dd($shopping_cart, $payment, $shopping_cart->with("affiliate")->find($shopping_cart->company_affiliated_id));
 
                 $update = ShoppingCart::where([['id', $shopping_cart->id],
                     ['payment_transaction_id', $payment->external_reference]])->
