95da9e81dd3d2f0823118ad9bc188434a24619d2
hsgarzon2020 <hsgarzon2020@gmail.com>
2020-09-15 22:59:07 -0500

    Update PaymentConfirmationController.php

--- app/Http/Controllers/Payment/PaymentConfirmationController.php
+++ app/Http/Controllers/Payment/PaymentConfirmationController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Payment;
 
 use App\Http\Controllers\Controller;
-use App\Models\ShoppingCart;
+use App\Mail\SendSuccessfulPaymentNotification;
 use App\Models\AffiliatedAccountService;
 use App\Models\AffiliatedContentAccountService;
+use App\Models\AfiliadoEmpresa;
 use App\Models\SequenceMoment;
+use App\Models\ShoppingCart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\SendSuccessfulPaymentNotification;
 use MercadoPago;
 
 class PaymentConfirmationController extends Controller
@@ -19,23 +20,23 @@ class PaymentConfirmationController extends Controller
         MercadoPago\SDK::setAccessToken(env('MERCADOPAGO_ACCESS_TOKEN'));
 
         // Consulta de pagos en estado 2 en la tabla shoppingCarts
-        $shopping_carts = ShoppingCart::with("affiliate")->where([
+        $shopping_carts = ShoppingCart::with("affiliate", "rating_plan", "shopping_cart_product")->where([
             ['payment_status_id', 2],
         ])->get();
 
-        foreach ($shopping_carts as $shopping_cart) {
+        foreach ($shopping_carts as $shoppingCart) {
 
             $filters = array(
-                "external_reference" => $shopping_cart->payment_transaction_id,
+                "external_reference" => $shoppingCart->payment_transaction_id,
             );
             $payments = MercadoPago\Payment::search($filters);
             $payment = end($payments);
 
             if ($payment->status == "approved") {
 
-                dd($shopping_cart, $payment, $shopping_cart->with("affiliate"));
+               // dd($shoppingCart, $payment, $shoppingCart->with("affiliate"));
 
-                $update = ShoppingCart::where([['id', $shopping_cart->id],
+                $update = ShoppingCart::where([['id', $shoppingCart->id],
                     ['payment_transaction_id', $payment->external_reference]])->
                     update(array(
                     'payment_status_id' => '3',
@@ -44,40 +45,19 @@ class PaymentConfirmationController extends Controller
                     'payment_method' => 'PSE',
                 ));
 
-                //if ($request->user('afiliadoempresa')) {
+                $afiliado_empresa = AfiliadoEmpresa::find($shoppingCart->affiliate->id);
 
-                    //$afiliado_empresa = $request->user('afiliadoempresa');
-                    $afiliado_empresa = $shopping_cart->company_affiliated_id;
-                    $shoppingCartsPay = ShoppingCart::
-                        with('rating_plan', 'shopping_cart_product')->
-                        where([
-                        ['company_affiliated_id', $shopping_cart->company_affiliated_id],
-                        ['payment_transaction_id', $payment->external_reference],
-                        ['payment_status_id', 3],
-                    ])->get();
+                $ratingPlan = $shoppingCart->rating_plan;
+                if ($ratingPlan) {
+                    //Iniciar el tiempo de acceso a las secuencias
+                    $this->addRatingPlanPaid($shoppingCart, $ratingPlan, $afiliado_empresa);
+                }
 
-                    foreach ($shoppingCartsPay as $shoppingCart) {
-                        $ratingPlan = $shoppingCart->rating_plan;
-                        if ($ratingPlan) {
-                            //Iniciar el tiempo de acceso a las secuencias
-                            $this->addRatingPlanPaid($shoppingCart, $ratingPlan, $afiliado_empresa);
-                        }
-                    }
-                //}
-                $transaction_date = ShoppingCart::select('payment_process_date')->where('payment_transaction_id', $payment->external_reference)->first();
                 //Envío correo de pago exitoso
-                //Mail::to($request->user('afiliadoempresa')->email)->send(
-                Mail::to($payment->payer->email)->send(
-                    new SendSuccessfulPaymentNotification($shoppingCart, $payment, $afiliado_empresa, $payment->transaction_amount, $transaction_date));
+                Mail::to($afiliado_empresa->email)->send(
+                    new SendSuccessfulPaymentNotification($shoppingCart, $payment, $afiliado_empresa, $payment->transaction_amount, $payment->date_approved));
                 return redirect()->route('tutor.products', ['empresa' => 'conexiones']);
-            } else {
-                $update = ShoppingCart::where([['id', $shopping_cart->id]])->update(
-                    array(
-                        'payment_status_id' => 5,
-                        //se debería almacenar en log los cambios de estado por este proceso
-                    )
-                );
-            }
+            } 
         }
     }
 
