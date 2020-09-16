95ff897e9c886d42b9d60b1683c6a63fdb38ff78
hsgarzon2020 <hsgarzon2020@gmail.com>
2020-09-16 18:31:16 -0500

    Update successfulPaymentNotification.blade.php

--- resources/views/vendor/notifications/successfulPaymentNotification.blade.php
+++ resources/views/vendor/notifications/successfulPaymentNotification.blade.php
@@ -43,13 +43,11 @@
                                         </span>
                                         <br>
                                         <span color="#0059a4" face="Roboto, sans-serif">
-                                            @if ({{$request->collection_id}})
-                                                <span style="color:#999999">Identificador de la transacción: {{$request->collection_id}}
-                                            
+                                            @if ($request->collection_id)
+                                                <span style="color:#999999">Identificador de la transacción: {{$request->collection_id}}</span>
                                             @else
-                                                <span style="color:#999999">Identificador de la transacción: {{$request->id}}
+                                                <span style="color:#999999">Identificador de la transacción: {{$request->id}}</span>
                                             @endif
-                                            </span>
                                             <br>
                                             <span style="color:#999999">Usuario: {{$afiliadoEmpresa->user_name}}
                                             </span>
