753bc926246c35b55bd25d40ed278e0a0ca1bff4
David Camacho <davithc01@gmail.com>
2020-10-14 21:45:10 -0500

    estilos pagina experiencias

--- resources/views/roles/student/available_experiences.blade.php
+++ resources/views/roles/student/available_experiences.blade.php
@@ -5,33 +5,35 @@
         <div class="content">
             <div ng-controller="availableExperiencesStudentCtrl" ng-init="init({{auth('afiliadoempresa')->user()->company_id()}},
                 {{$sequence_id}}, {{$account_service_id}})">
-                <div class="row">
+                <div class="row conx-page">
                     <div id="loading" class="z-index-10 position-absolute card card-body p-10 w-100 text-align" ng-hide="sequence"
                       style="min-height: 100%;" >
                       cargando...
                     </div>
-                    <div class="col-12 col-md-8 row" ng-show="sequence">
-                        <div class="h-100 h-md-35 w-100">
+                    <div class="col-12 col-lg-8 row" ng-show="sequence">
+                        <div class="h-100 h-lg-20 w-100">
                             <iframe class="col-12" id="vimeo-player" frameborder="0" width="100%" height="100%" refreshable="sequence.url_vimeo"
                             
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                             </iframe>
                             <div class="card-body d-flex col-12 row no-gutters">
                                 <div class="col-auto">
-                                    <img src="/@{{sequence.url_image}}" style="width:auto; height:auto;">
+                                    <img src="/@{{sequence.url_image}}" style="width:auto; height:auto; min-width: 90px;">
                                 </div>
                                 <div class="col-8">
                                     <label class="ml-4 card-title">@{{momentPart.title}}</label>
+                                    <p>
                                     <label class="ml-4 card-title fs-0">Momento @{{moment.order + '. ' + moment.moment_name}}</label>
+                                    </p>
                                     <h6 class="ml-4 card-title">@{{sequence.name}}</h6>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class=" card col-12 col-md-4 row card-body shadow-none">
+                    <div class=" card col-12 col-lg-4 row card-body shadow-none">
                         <div class="card-body bg-light mb-1" ng-repeat = "moment in moments">
-                            <div class="row ">
-                                <div class="col-2-3 col-md-5">
+                            <div class="row " style="min-height: 100px;">
+                                <div class="col-2-3 col-lg-5">
                                     <img src="/@{{moment.url_image_experience || 'images/icons/NoImageAvailable.jpeg'}}" width="100%" height="auto">
                                 </div>
                                 <div class="col-7">
